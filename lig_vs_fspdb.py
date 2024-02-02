68fb77950fc117aeee5a466c7f4c31c2d30b7548
Gabi <lucasgabrielgarcia1@gmail.com>
2024-02-02 01:34:49 -0300

    fixed named aplied, dont run if res folder already exists

--- lig_vs_fspdb.py
+++ lig_vs_fspdb.py
@@ -4,6 +4,7 @@ import random
 import string
 import argparse
 import pandas as pd
+import os
 
 def generate_random_name(length=3):
     """Generate a random name of specified length."""
@@ -14,21 +15,26 @@ def run_autodock_protocol(smiles, ligand_name, pdb_code):
     """Run the Autodock protocol with the given parameters."""
     try:
         command = ["./generate_pdb_with_ref.sh", smiles, ligand_name, pdb_code]
-        print(command)
         subprocess.run(command, check=True)  # Add check=True to raise exception on non-zero exit code
     except subprocess.CalledProcessError as e:
         print(f"Error running Autodock protocol for PDB code {pdb_code}: {e}")
 
 def main(csv_file):
     """Main function to read CSV file and execute Autodock protocol."""
+    cwd = os.getcwd()
     df = pd.read_csv(csv_file)
     df_smiles = df.dropna(subset=["smiles"])
     smiles = df_smiles['smiles'].tolist()
     pdbs = df['pdb'].tolist()
-    for smile in smiles:
-        ligand_name = generate_random_name()
+    names = df['name'].tolist()
+    name_smile = zip(names,smiles)
+    for name,smile in name_smile:
         for pdb in pdbs:
-            run_autodock_protocol(smile, ligand_name, pdb)
+            path_to_check = os.path.join(cwd, name + "_" + pdb)
+            print(path_to_check)
+            if not os.path.exists(path_to_check):
+                run_autodock_protocol(smile, name, pdb)
+            
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Makes all the dockings')
